d68f0a71b47668c74085a82acd8326c313865f4c
Ashley <a.clarkem@gmail.com>
2014-06-26 11:20:23 -0600

    finished layout problem

--- lib/html_generator.rb
+++ lib/html_generator.rb
@@ -1,25 +1,39 @@
 class HTMLGenerator
 
-  def section(string)
-  "<section>#{string}</section>"
+  def initialize(layout = nil)
+    @layout = layout
+  end
 
+  def section(string)
+    if @layout == nil
+    "<section>#{string}</section>"
+    else
+      "<main><section>#{string}</section></main>"
+    end
   end
 
   def unordered_list (list_items)
-    #unecessarily complicated? Try simplifying it
+    if @layout == nil
    array = []
     list_items.each do |item|
       array.push(item)
     end
     list = array.join("</li><li>")
     "<ul><li>#{list}</li></ul>"
+    else
+      "<div><ul><li>#{list_items[0]}</li></ul></div>"
+    end
   end
 
   def button(button, options = nil)
-   if options == nil
+   if @layout == nil
+    if options == nil
     "<button>#{button}</button>"
-  else
+    else
     "<button class='#{options[:class]}'>#{button}</button>"
-  end
+    end
+   else
+     "<section><button>#{button}</button></section>"
+   end
   end
 end
